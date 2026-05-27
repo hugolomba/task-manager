00999574a9013673398d3323b4efad8bf5848a1b
Hugo Lomba <98366477+hugolomba@users.noreply.github.com>
2026-05-27 15:47:09 +0100

    add Task POJO

--- src/main/java/com/hugo/taskmanager/entity/Task.java
+++ src/main/java/com/hugo/taskmanager/entity/Task.java
@@ -1,4 +1,19 @@
 package com.hugo.taskmanager.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+// lombok annotations
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Task {
+    private Long id;
+    private String title;
+    private String description;
+    private boolean completed;
+    private LocalDateTime createdAt;
 }
