1307a60f6e96fb3f1fd4590c58bcaa7cc65aba02
Hugo Lomba <98366477+hugolomba@users.noreply.github.com>
2026-05-27 16:05:35 +0100

    Created a Post request to creat a new task

--- src/main/java/com/hugo/taskmanager/controller/TaskController.java
+++ src/main/java/com/hugo/taskmanager/controller/TaskController.java
@@ -1,10 +1,9 @@
 package com.hugo.taskmanager.controller;
 
 import com.hugo.taskmanager.entity.Task;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,4 +19,17 @@ public class TaskController {
     public List<Task> getAllTasks() {
         return tasks;
     }
+
+    @PostMapping
+    //@RequestBody transform the JSON coming in the request body in a Java object
+    public Task createTask(@RequestBody Task task)  {
+        task.setId(nextId++);
+        task.setCreatedAt(LocalDateTime.now());
+        task.setCompleted(false);
+        tasks.add(task);
+
+        return task;
+    }
+
+
 }
--- src/main/java/com/hugo/taskmanager/entity/Task.java
+++ src/main/java/com/hugo/taskmanager/entity/Task.java
@@ -14,6 +14,6 @@ public class Task {
     private Long id;
     private String title;
     private String description;
-    private boolean completed;
+    private Boolean completed;
     private LocalDateTime createdAt;
 }
