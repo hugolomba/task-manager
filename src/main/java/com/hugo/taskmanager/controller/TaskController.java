ff3a5ced6a7235c953b9380f3b85af87c27425b1
Hugo Lomba <98366477+hugolomba@users.noreply.github.com>
2026-05-29 19:56:10 +0100

    Refactored the code to make the TaskController class use the TaskRepository bean

--- src/main/java/com/hugo/taskmanager/controller/TaskController.java
+++ src/main/java/com/hugo/taskmanager/controller/TaskController.java
@@ -1,6 +1,9 @@
 package com.hugo.taskmanager.controller;
 
 import com.hugo.taskmanager.entity.Task;
+import com.hugo.taskmanager.repository.TaskRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -12,28 +15,31 @@ import java.util.List;
 public class TaskController {
 
 //    temporary in-memory task list
-    private List<Task> tasks = new ArrayList<>();
-    private Long nextId = 1L;
+//    private List<Task> tasks = new ArrayList<>();
+//    private Long nextId = 1L;
+
+    private final TaskRepository taskRepository;
+
+    // Dependency Injection of the task repository
+    public TaskController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
 
     @GetMapping
     public List<Task> getAllTasks() {
-        return tasks;
+        return taskRepository.findAll();
     }
 
     @GetMapping("/{id}")
-    public Task getTaskById (@PathVariable Long id) {
-        return tasks.stream().filter(task -> task.getId().equals(id)).findFirst().orElse(null);
+    public ResponseEntity<Task> getTaskById (@PathVariable Long id) {
+        return taskRepository.findById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
     //@RequestBody transform the JSON coming in the request body in a Java object
-    public Task createTask(@RequestBody Task task)  {
-        task.setId(nextId++);
-        task.setCreatedAt(LocalDateTime.now());
-        task.setCompleted(false);
-        tasks.add(task);
-
-        return task;
+    public ResponseEntity<Task> createTask(@RequestBody Task task)  {
+        Task savedTask = taskRepository.save(task);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedTask);
     }
 
 // version without stream
@@ -54,24 +60,42 @@ public class TaskController {
 //        return null;
 //    }
 
+//    @PutMapping("/{id}")
+//    public Task updateTask (@PathVariable Long id, @RequestBody Task updatedTask) {
+//        Task foundTask = tasks.stream().filter(task -> task.getId().equals(id)).findFirst().orElse(null);
+//
+//        updatedTask.setId(id);
+//        updatedTask.setCreatedAt(foundTask.getCreatedAt());
+//
+//        int index = tasks.indexOf(foundTask);
+//
+//        tasks.set(index, updatedTask);
+//
+//        return updatedTask;
+//    }
+
     @PutMapping("/{id}")
-    public Task updateTask (@PathVariable Long id, @RequestBody Task updatedTask) {
-        Task foundTask = tasks.stream().filter(task -> task.getId().equals(id)).findFirst().orElse(null);
+    public ResponseEntity<Task> updateTask (@PathVariable Long id, @RequestBody Task updatedTask) {
 
-        updatedTask.setId(id);
-        updatedTask.setCreatedAt(foundTask.getCreatedAt());
-
-        int index = tasks.indexOf(foundTask);
-
-        tasks.set(index, updatedTask);
-
-        return updatedTask;
+        return taskRepository.findById(id)
+                .map(task -> {
+                    task.setTitle(updatedTask.getTitle());
+                    task.setDescription(updatedTask.getDescription());
+                    task.setCompleted((updatedTask.getCompleted()));
+                    Task savedTask = taskRepository.save(task);
+                    return ResponseEntity.ok(savedTask);
+                }).orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
-    public void deleteTask(@PathVariable Long id) {
-        tasks.removeIf(task -> task.getId().equals(id));
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
 
+        return taskRepository.findById(id)
+                .map(task -> {
+                    taskRepository.delete(task);
+                    return ResponseEntity.ok().<Void>build();
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
 
 }
