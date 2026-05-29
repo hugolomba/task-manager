15ec1aa0ecaf2115dfa1e291c36cad6f91b7c0ef
Hugo Lomba <98366477+hugolomba@users.noreply.github.com>
2026-05-29 19:13:42 +0100

    Add the annotations to define the tables. We have the entity class that gonna be the object in the database

--- src/main/java/com/hugo/taskmanager/entity/Task.java
+++ src/main/java/com/hugo/taskmanager/entity/Task.java
@@ -1,19 +1,40 @@
 package com.hugo.taskmanager.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "tasks")
+
 // lombok annotations
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String title;
+
+    @Column(length = 500)
     private String description;
+
+    @Column(nullable = false)
     private Boolean completed;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
+
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
